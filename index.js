3d463d91ee3bbde870c275c2c38492fd035bba9b
unknown <mahamuda3052@gmail.com>
2022-10-26 05:43:57 +0600

    creat /courses/:id link to get specific id data

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get('/',(req,res)=>{
 app.get('/courses',(req,res)=>{
     res.send(courses)
 })
+// get courses by id
+app.get('/courses/:id',(req,res)=>{
+    const id = req.params.id;
+    const selectId = courses.find(course=>course.id===id)
+    res.send(selectId)
+})
 app.listen(port,()=>{
     console.log('learn programming server runing on port',port)
 })
