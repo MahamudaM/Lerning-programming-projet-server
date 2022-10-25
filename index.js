cf28bcc153a0adc956b09cd0510e9523abadbcb5
unknown <mahamuda3052@gmail.com>
2022-10-25 17:26:50 +0600

    use corse for send data client server

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
-
-
+const courses = require("./courses.json")
+const cors = require('cors')
+app.use(cors())
 app.get('/',(req,res)=>{
     res.send('running api'); 
+});
+app.get('/courses',(req,res)=>{
+    res.send(courses)
 })
 app.listen(port,()=>{
     console.log('learn programming server runing on port',port)
